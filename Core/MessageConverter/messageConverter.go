a23238dc3d48d7aebfa19c249088ef9553a25d60
LukasRolle <l.rolle@student.fontys.nl>
2016-11-11 13:54:57 +0100

    finished the data types the message converter can convert and fixed some errors, added unit test

--- Core/MessageConverter/messageConverter.go
+++ Core/MessageConverter/messageConverter.go
@@ -5,7 +5,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"math"
+	"strconv"
 )
 
 type conversionType int
@@ -20,7 +20,7 @@ const (
 )
 
 type MessageConverter interface {
-	ConvertSingleValue(payload []byte, conversion int) string
+	ConvertSingleValue(payload []byte, conversion int) (string, error)
 }
 
 type messageConverter struct {
@@ -35,95 +35,117 @@ func New() MessageConverter {
 // representation of the received value. The conversion int refers to the type of
 // the value that has been received. The types that can be input are uint(0),
 // int(1), float(2), string(3) or bool(4).
-func (m *messageConverter) ConvertSingleValue(payload []byte, conversion int) string {
-	bits := binary.LittleEndian.Uint64(payload)
-	float := math.Float64frombits(bits)
-	return fmt.Sprintf("%f64 %d", float, conversionFloat)
+// Returns an error when an invalid type is chosen.
+func (m *messageConverter) ConvertSingleValue(payload []byte, conversion int) (string, error) {
+	var result string
+	var err error
+	switch conversionType(conversion) {
+	case conversionUint:
+		result, err = convertUint(payload)
+	case conversionInt:
+		result, err = convertInt(payload)
+	case conversionFloat:
+		result, err = convertFloat(payload)
+	case conversionString:
+		result, err = convertString(payload)
+	case conversionBool:
+		result, err = convertBool(payload)
+	default:
+		err = errors.New("Invalid Conversion type")
+		return "", err
+	}
+	return result, err
 }
 
 // Conversion method for the uint type, which type of uint(8, 16, 32, 64) will
 // be determined by the length of the byte slice. Returns a string representation
 // of the resulting uint.
-func convertUint(payload []byte) string {
+func convertUint(payload []byte) (string, error) {
 	var result string
 	var err error
 	switch len(payload) {
 
 	case 1: //uint8
-		return string(payload[0])
+		return strconv.FormatUint(uint64(payload[0]), 10), nil
 	case 2: //uint16
-
+		return strconv.FormatUint(uint64(binary.BigEndian.Uint16(payload)), 10), nil
 	case 4: //uint32
-
+		return strconv.FormatUint(uint64(binary.BigEndian.Uint32(payload)), 10), nil
 	case 8: //uint64
-
+		return strconv.FormatUint(binary.BigEndian.Uint64(payload), 10), nil
 	default:
+		result = ""
 		err = errors.New("illegal length of payload for a uint type")
 	}
-	if err != nil {
-		fmt.Println("binary.Read failed:", err)
-	}
-	return result
+	return result, err
 }
 
 // Conversion method for the int type, which type of int(8, 16, 32, 64) will
 // be determined by the length of the byte slice. Returns a string representation
 // of the resulting int.
-func convertInt(payload []byte) string {
-	var result string
+func convertInt(payload []byte) (string, error) {
+
 	var err error
+	buf := bytes.NewReader(payload)
 	switch len(payload) {
 	case 1: //int8
-
+		var integer int8
+		err = binary.Read(buf, binary.BigEndian, &integer)
+		return fmt.Sprintf("%d", integer), err
 	case 2: //int16
-
+		var integer int16
+		err = binary.Read(buf, binary.BigEndian, &integer)
+		return fmt.Sprintf("%d", integer), err
 	case 4: //int32
-
+		var integer int32
+		err = binary.Read(buf, binary.BigEndian, &integer)
+		return fmt.Sprintf("%d", integer), err
 	case 8: //int64
-
+		var integer int64
+		err = binary.Read(buf, binary.BigEndian, &integer)
+		return fmt.Sprintf("%d", integer), err
 	default:
 		err = errors.New("illegal length of payload for an int type")
+		return "", err
 	}
-	if err != nil {
-		fmt.Println("binary.Read failed:", err)
-	}
-	return result
 }
 
 // Conversion method for the float type, which type of float(32, 64) will
 // be determined by the length of the byte slice. Returns a string representation
 // of the resulting float.
-func convertFloat(payload []byte) string {
-	var float interface{}
+func convertFloat(payload []byte) (string, error) {
 	var err error
 	buf := bytes.NewReader(payload)
 	switch len(payload) {
 	case 4: //float32
-		float = float.(float32)
+		var float float32
+		err = binary.Read(buf, binary.BigEndian, &float)
+		return fmt.Sprintf("%E", float), err
 	case 8: //float64
-		float = float.(float64)
+		var float float64
+		err = binary.Read(buf, binary.BigEndian, &float)
+		return fmt.Sprintf("%E", float), err
 	default:
 		err = errors.New("illegal length of payload for a float type")
+		return "", err
 	}
-	err = binary.Read(buf, binary.LittleEndian, &float)
-	if err != nil {
-		fmt.Println("binary.Read failed:", err)
-	}
-	return fmt.Sprintf("%f", float)
 }
 
 // Conversion method for the string type.
-func convertString(payload []byte) string {
-	return string(payload)
+func convertString(payload []byte) (string, error) {
+	return string(payload), nil
 }
 
 // Conversion method for the bool type.
 // It is expected that the length of payload is never greater than  one.
 // Result will be either "true" or "false" as a string.
-func convertBool(payload []byte) string {
+func convertBool(payload []byte) (string, error) {
+	if len(payload) > 1 {
+		return "", errors.New("invalid payload length")
+	}
 	if payload[0] > 0 {
-		return "true"
+		return "true", nil
 	} else {
-		return "false"
+		return "false", nil
 	}
 }
---
+++ Core/MessageConverter/messageConverter_test.go
@@ -0,0 +1,158 @@
+package messageConverter
+
+import "testing"
+
+var fibTests = []struct {
+	n        int // input
+	expected int // expected result
+}{
+	{1, 1},
+	{2, 1},
+	{3, 2},
+	{4, 3},
+	{5, 5},
+	{6, 8},
+	{7, 13},
+}
+
+var invalidByteArray = []byte{0x00, 0x00, 0x00}
+var validByteArrays = [][]byte{
+	{0xFF},
+	{0x00},
+	{0x99},
+	{0x99, 0x23},
+	{0xA2, 0xCB},
+	{0xA2, 0xCB, 0xF2, 0xC3},
+	{0x11, 0x11, 0x11, 0x11},
+	{0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0},
+	{0x3B, 0x17, 0x24, 0x92, 0xA8, 0xF2, 0x9D, 0x2E}}
+
+func TestUintTypes(t *testing.T) {
+	var testData = []struct {
+		expectedResult string // expectedResult
+		payload        []byte // input
+	}{
+		{"255", validByteArrays[0]},
+		{"0", validByteArrays[1]},
+		{"153", validByteArrays[2]},
+		{"39203", validByteArrays[3]},
+		{"41675", validByteArrays[4]},
+		{"2731274947", validByteArrays[5]},
+		{"286331153", validByteArrays[6]},
+		{"1311768467463790320", validByteArrays[7]},
+		{"4257912185020390702", validByteArrays[8]},
+	}
+	messageConverter := New()
+	for _, item := range testData {
+		result, _ := messageConverter.ConvertSingleValue(item.payload, 0)
+		if result != item.expectedResult {
+			t.Errorf("Expected %s, was %s with payload %v", item.expectedResult, result, item.payload)
+		}
+	}
+	_, err := messageConverter.ConvertSingleValue(invalidByteArray, 0)
+	if err == nil {
+		t.Errorf("Uint message length should be invalid and produce an error %v", invalidByteArray)
+	}
+}
+
+func TestIntTypes(t *testing.T) {
+	var testData = []struct {
+		expectedResult string // expectedResult
+		payload        []byte // input
+	}{
+		{"-1", validByteArrays[0]},
+		{"0", validByteArrays[1]},
+		{"-103", validByteArrays[2]},
+		{"-26333", validByteArrays[3]},
+		{"-23861", validByteArrays[4]},
+		{"-1563692349", validByteArrays[5]},
+		{"286331153", validByteArrays[6]},
+		{"1311768467463790320", validByteArrays[7]},
+		{"4257912185020390702", validByteArrays[8]},
+	}
+	messageConverter := New()
+	for _, item := range testData {
+		result, _ := messageConverter.ConvertSingleValue(item.payload, 1)
+		if result != item.expectedResult {
+			t.Errorf("Expected %s, was %s with payload %v", item.expectedResult, result, item.payload)
+		}
+	}
+	_, err := messageConverter.ConvertSingleValue(invalidByteArray, 1)
+	if err == nil {
+		t.Errorf("Int message length should be invalid and produce an error %v", invalidByteArray)
+	}
+}
+func TestFloatTypes(t *testing.T) {
+	var testData = []struct {
+		expectedResult string // expectedResult
+		payload        []byte // input
+	}{
+		{"-5.528029E-18", validByteArrays[5]},
+		{"1.144374E-28", validByteArrays[6]},
+		{"5.626349E-221", validByteArrays[7]},
+		{"4.785832E-24", validByteArrays[8]},
+	}
+	messageConverter := New()
+	for _, item := range testData {
+		result, _ := messageConverter.ConvertSingleValue(item.payload, 2)
+		if result != item.expectedResult {
+			t.Errorf("Expected %s, was %s with payload %v", item.expectedResult, result, item.payload)
+		}
+	}
+	_, err := messageConverter.ConvertSingleValue(invalidByteArray, 2)
+	if err == nil {
+		t.Errorf("Float message length should be invalid and produce an error %v", invalidByteArray)
+	}
+}
+
+func TestStringTypes(t *testing.T) {
+	var testData = []struct {
+		expectedResult string // expectedResult
+		payload        []byte // input
+	}{
+		{"Test", []byte{0x54, 0x65, 0x73, 0x74}},
+		{"Bits", []byte{0x42, 0x69, 0x74, 0x73}},
+		{"Bytes", []byte{0x42, 0x79, 0x74, 0x65, 0x73}},
+		{"supercalifragilisticexpialidocious", []byte{0x73, 0x75, 0x70, 0x65, 0x72,
+			0x63, 0x61, 0x6C, 0x69, 0x66, 0x72, 0x61, 0x67, 0x69, 0x6C, 0x69, 0x73,
+			0x74, 0x69, 0x63, 0x65, 0x78, 0x70, 0x69, 0x61, 0x6C, 0x69, 0x64, 0x6F,
+			0x63, 0x69, 0x6F, 0x75, 0x73}},
+	}
+	messageConverter := New()
+	for _, item := range testData {
+		result, _ := messageConverter.ConvertSingleValue(item.payload, 3)
+		if result != item.expectedResult {
+			t.Errorf("Expected %s, was %s with payload %v", item.expectedResult, result, item.payload)
+		}
+	}
+}
+
+func TestBooleanTypes(t *testing.T) {
+	var testData = []struct {
+		expectedResult string // expectedResult
+		payload        []byte // input
+	}{
+		{"true", []byte{0xFF}},
+		{"true", []byte{0x01}},
+		{"false", []byte{0x00}},
+	}
+	messageConverter := New()
+	for _, item := range testData {
+		result, _ := messageConverter.ConvertSingleValue(item.payload, 4)
+		if result != item.expectedResult {
+			t.Errorf("Expected %s, was %s with payload %v", item.expectedResult, result, item.payload)
+		}
+	}
+	_, err := messageConverter.ConvertSingleValue(invalidByteArray, 4)
+	if err == nil {
+		t.Errorf("Boolean message length should be invalid and produce an error %v", invalidByteArray)
+	}
+}
+
+func TestInvalidType(t *testing.T) {
+	messageConverter := New()
+	_, err := messageConverter.ConvertSingleValue(invalidByteArray, 5)
+	if err == nil {
+		t.Errorf("Boolean message length should be invalid and produce an error %v", invalidByteArray)
+	}
+}
