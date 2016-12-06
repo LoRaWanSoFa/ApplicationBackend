3613f02b216a9d81ba9f554112d5ad195c7607a5
sander lemans <sander lemans>
2016-12-06 11:57:28 +0100

    oops forgot something

--- Components/message.go
+++ Components/message.go
@@ -1,37 +1,40 @@
 package components
 
+import "log"
+
 type MessageUplinkI interface {
 	AddPayload(p []byte, s Sensor)
-	GetPayloads() []messagePayload
+	GetPayloads() []messagePayloadI
+	GetId() int64
 }
 
 type MessageUplink struct {
 	Id       int64
 	Time     string
 	DevEUI   string // or [8]byte or types.DevEUI
-	Payloads []messagePayload
+	Payloads []messagePayloadI
 }
 
 func NewMessageUplink(id int64, devEUI string) MessageUplinkI {
-	var messageI MessageUplinkI
-	var message MessageUplink
-	message = MessageUplink{Id: id, DevEUI: devEUI}
-	message.Payloads = make([]messagePayload, 0)
-	messageI = message
-	return messageI
+	message := new(MessageUplink)
+	message.Id = id
+	message.DevEUI = devEUI
+	message.Payloads = make([]messagePayloadI, 0)
+	return message
 }
 
-func (m MessageUplink) AddPayload(p []byte, s Sensor) {
-	mp := messagePayload{payload: p, sensor: s}
+func (m *MessageUplink) AddPayload(p []byte, s Sensor) {
+	mp := new(messagePayloadByte)
+	mp.Payload = p
+	mp.Sensor = s
+	log.Printf("Payload with payloads: %+v", mp)
 	m.Payloads = append(m.Payloads, mp)
 }
 
-func (m MessageUplink) GetPayloads() []messagePayload {
+func (m *MessageUplink) GetPayloads() []messagePayloadI {
 	return m.Payloads
 }
 
-type messagePayload struct {
-	id      int64
-	payload []byte
-	sensor  Sensor
+func (m *MessageUplink) GetId() int64 {
+	return m.Id
 }
