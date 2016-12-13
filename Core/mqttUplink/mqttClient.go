6036dcef27cd56a453c13c2448269b50160017c4
LukasRolle <l.rolle@student.fontys.nl>
2016-12-13 11:05:09 +0100

    add initial version of mqttClient

---
+++ Core/mqttUplink/mqttClient.go
@@ -0,0 +1,74 @@
+package mqttUplink
+
+import (
+	"errors"
+	"os"
+
+	goLog "log"
+
+	"github.com/LoRaWanSoFa/LoRaWanSoFa/Components"
+	"github.com/LoRaWanSoFa/LoRaWanSoFa/Core/distributor"
+	"github.com/LoRaWanSoFa/ttn/core"
+	"github.com/LoRaWanSoFa/ttn/core/types"
+	"github.com/LoRaWanSoFa/ttn/mqtt"
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/text"
+)
+
+type MqttClient interface {
+	Connect() error
+	Disconnect()
+	GetClient() mqtt.Client
+}
+
+type mqttClient struct {
+	client mqtt.Client
+}
+
+func New() MqttClient {
+	client := new(mqttClient)
+	return client
+}
+
+var creator = NewMessageCreator()
+var dist = distributor.New()
+
+func uplinkMessageHandler(client mqtt.Client, appEUI types.AppEUI, devEUI types.DevEUI, req core.DataUpAppReq) {
+	if len(req.Payload) > 0 {
+		flag := req.Payload[0]
+		if flag>>4 == 1 {
+			//TODO: implement header handling
+		} else if flag>>5 == 1 {
+			message, err := creator.CreateMessage(req.Payload, devEUI.Bytes())
+			if err != nil {
+				goLog.Fatal(err)
+			} else {
+				dist.InputUplink(message)
+			}
+		} else {
+			err := errors.New("No valid flag")
+			goLog.Fatal(err)
+		}
+	}
+}
+
+func (m *mqttClient) Connect() error {
+	log.SetHandler(text.New(os.Stderr))
+	mqttConfig := components.GetConfiguration().Mqtt
+	ctx := log.WithField("distributorClient", "mqtt-distributorClient")
+	m.client = mqtt.NewClient(ctx, "ttnctl", mqttConfig.AppEUI, mqttConfig.Password, mqttConfig.Address)
+	if err := m.client.Connect(); err != nil {
+		return errors.New("Could not connect")
+	}
+	u := uplinkMessageHandler
+	m.client.SubscribeUplink(u)
+	return nil
+}
+
+func (m *mqttClient) Disconnect() {
+	m.client.Disconnect()
+}
+
+func (m *mqttClient) GetClient() mqtt.Client {
+	return m.client
+}
---
+++ Core/mqttUplink/mqttClient_test.go
@@ -0,0 +1,42 @@
+package mqttUplink
+
+import (
+	"testing"
+
+	components "github.com/LoRaWanSoFa/LoRaWanSoFa/Components"
+	"github.com/LoRaWanSoFa/LoRaWanSoFa/Core/distributor"
+	"github.com/LoRaWanSoFa/ttn/core"
+	"github.com/LoRaWanSoFa/ttn/core/types"
+)
+
+type mockDistributor struct {
+	message components.MessageUplinkI
+}
+
+func newMockDistributor() distributor.Distributor {
+	return new(mockDistributor)
+}
+
+var mClient = New()
+
+func (m *mockDistributor) InputUplink(message components.MessageUplinkI) (components.MessageUplinkI, error) {
+	m.message = message
+	return message, nil
+}
+func (m *mockDistributor) InputDownlink(message components.MessageDownLink) {
+}
+
+func TestUplinkMessageHandler(t *testing.T) {
+	dist = newMockDistributor()
+	mClient.Connect()
+	payload := core.DataUpAppReq{}
+	payload.Payload = []byte{0x20, 0x42, 0x22, 0xEC, 0x25, 0xC2, 0x93, 0xDE, 0xD8, 0x01}
+	devEui := types.DevEUI{0x00, 0x00, 0x00, 0x00, 0xAB, 0xCD, 0xEF, 0x12}
+
+	uplinkMessageHandler(mClient.GetClient(), types.AppEUI{}, devEui, payload)
+	payloads := dist.(*mockDistributor).message.GetPayloads()
+	if len(payloads) != 3 {
+		t.Errorf("payload should contain 3 arguments, but had %d.", len(payloads))
+	}
+	//	uplinkMessageHandler(client, appEUI, devEUI, req)
+}
