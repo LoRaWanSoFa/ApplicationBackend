b8934b4017083c59e1df2d65065f7c272d96a44c
LukasRolle <l.rolle@student.fontys.nl>
2016-12-15 16:35:53 +0100

    remove commented out code

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 )
 
 func main() {
-
 	router := webserver.NewRouter()
-
 	log.Fatal(http.ListenAndServe(":8080", router))
-
-	//test := MessageConverter.New()
-
-	//bytes := []byte{0x3F, 0xB0, 0xFC, 0x00, 0xAA, 0xA0, 0x84, 0x41}
-	//fmt.Println(test.ConvertSingleValue(bytes, 2))
 }
